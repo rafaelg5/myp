b37ce69b9f53f4e1fc1d870a612e761f129810b8
rafaelg5 <rafael.garcia.g5@gmail.com>
2015-09-15 17:19:59 -0500

    Modificaciones a la clase Token

--- src/myp/Token.java
+++ src/myp/Token.java
@@ -2,41 +2,116 @@ package myp;
 
 public class Token {
     
-    private final int LEFT_RIGHT = 1;
-    private final int RIGHT_LEFT = 2;
+    /*
+     * Clase Privada Operador para Token.
+     */
+    private class Operator{
+	
+	public int precedence;
+	public OperatorArity arity;
+	public OperatorAssociativity associativity;
+
+	/*
+	 * Constructor de un operador de token.
+	 */
+	public Operator(int p, OperatorArity a, 
+			OperatorAssociativity as){
+	    precedence = p;
+	    arity = a;
+	    associativity = as;
+	}
+    }    
+
+    private final Operator OPERATORS[] = {    
+	new Operator(2, OperatorArity.BINARY, OperatorAssociativity.RIGHT),	
+	new Operator(3, OperatorArity.UNARY, OperatorAssociativity.RIGHT),
+	new Operator(4, OperatorArity.BINARY, OperatorAssociativity.LEFT),
+	new Operator(4, OperatorArity.BINARY, OperatorAssociativity.LEFT),    
+	new Operator(5, OperatorArity.BINARY, OperatorAssociativity.LEFT),
+	new Operator(5, OperatorArity.BINARY, OperatorAssociativity.LEFT),
+    };
+    
+    public enum OperatorArity{UNARY, BINARY}
+
+    public enum OperatorAssociativity{LEFT, RIGHT}
 
     public enum TokenType{
 	NUMBER, OPERATOR, VARIABLE, FUNCTION, 
 	LEFT_PARENTHESIS, RIGHT_PARENTHESIS, UNKNOWN        
     }
 
-    String element;
-    TokenType type;
-    int opAssoc;
-    int opPrec;
+    private String element;
+    private TokenType type;    
+    private Operator operator;
 
-    public Token(String e, TokenType t, int opAssoc, int opPrec){
-	if(t == TokenType.OPERATOR){
-	    if(opAssoc < 1 && opAssoc > 2)
-		throw new IllegalArgumentException("Argumento incorrecto");
-
-	    if(opPrec < 2 && opPrec > 4)
-		throw new IllegalArgumentException("Argumento incorrecto");	
-	}
+    /**
+     * Constructor que recibe una cadena y un tipo de token.
+     * @param e el elemento del token.
+     * @param t el tipo de token.
+     */
+    public Token(String e, TokenType t){
+        
+    }
+    
+    /**
+     * Constructor que recibe una cadena, el tipo de token y 
+     * el tipo de operador.
+     * @param e el elemento del token.
+     * @param t el tipo de token.
+     * @param i el tipo de operador que es el token.
+     */
+    public Token(String e, TokenType t, int i){
 	
-	element = e;
-	type = t;
-	this.opAssoc = opAssoc;
-	this.opPrec = opPrec;
-    }   
-
-    public String getElement(){
-	return this.element;
     }
 
+    /**
+     * Regresa el elemento del token.
+     * @return el elemento.
+     */
+    public String getElement(){
+	return null;
+    }
+
+    /**
+     * Regresa el tipo del token.
+     * @return el tipo del token.
+     */
     public TokenType getType(){
-	return this.type;
+	return null;
+    }
+
+    /**
+     * Checa si un token es operador.
+     * @return <tt>true</tt> si es operador, <tt>false</tt> en otro caso.
+     */
+    public boolean isOperator(){
+	return false;
+    }    
+
+    /**
+     * Regresa la precedencia del operador.
+     * @return la precedencia del operador.
+     */
+    public int getOperatorPrecedence(){
+	return -1;
+    }
+
+    
+    /**
+     * Regresa la aridad del operador.
+     * @return la aridad.
+     */
+    public OperatorArity getOperatorArity(){
+	return null;
+    }
+
+    
+    /**
+     * Regresa la asociatividad del operador.
+     * @return la asociatividad.
+     */
+    public OperatorAssociativity getOperatorAssociativity(){
+	return null;
     }
 }
-
 
