2d887a90a2d7f0eea4b81720a9eeba9409cbe2c4
rafaelg5 <rafael.garcia.g5@gmail.com>
2015-09-21 00:40:38 -0500

    Grafica expresiones dada. La interfaz gráfica aún es un desastre.

--- src/myp/Graph.java
+++ src/myp/Graph.java
@@ -11,15 +11,28 @@ import java.awt.event.ActionEvent;
 import java.awt.Color;
 import java.awt.geom.Line2D;
 
-public class Graph extends JFrame {
+public class Graph extends JFrame implements ChangeListener{
     
     private int graphWidth;
     private int graphHeight;
-    private ArrayList<Coordinates> coordinates;
-    private int domain;
     private int unitX;
-    private int codomain;
     private int unitY;
+    
+    private ArrayList<Coordinates> coordinates;   
+
+    private double minX;
+    private double minY;
+    private double maxX;
+    private double maxY;
+
+    private Controller c;
+
+    private JSpinner spinnerW;
+    private JSpinner spinnerH;
+    private JSpinner spinnerX1;
+    private JSpinner spinnerX2;
+    private JSpinner spinnerY1;
+    private JSpinner spinnerY2;
 
     private class Coordinates {
 	
@@ -64,96 +77,131 @@ public class Graph extends JFrame {
         final JTextArea tA = new JTextArea(35, 50);
 
         SpinnerNumberModel widthModel = 
-	    new SpinnerNumberModel(320, 320, 1080, 10);
-        final JSpinner spinnerW = new JSpinner(widthModel);
+	    new SpinnerNumberModel(640, 320, 1366, 10);
+	spinnerW = new JSpinner(widthModel);
         SpinnerNumberModel heightModel = 
-	    new SpinnerNumberModel(240, 240, 620, 10);
-        final JSpinner spinnerH = new JSpinner(heightModel);
+	    new SpinnerNumberModel(500, 240, 768, 10);
+        spinnerH = new JSpinner(heightModel);
         SpinnerNumberModel x1Model = 
-	    new SpinnerNumberModel(-5.0, -80.0, 79.0, 0.1);
-        final JSpinner spinnerX1 = new JSpinner(x1Model);
+	    new SpinnerNumberModel(-10.0, -80.0, 79.0, 0.1);
+        spinnerX1 = new JSpinner(x1Model);
         SpinnerNumberModel x2Model = 
-	    new SpinnerNumberModel(5.0, -79.0, 80.0, 0.1);
-        final JSpinner spinnerX2 = new JSpinner(x2Model);
+	    new SpinnerNumberModel(10.0, -79.0, 80.0, 0.1);
+        spinnerX2 = new JSpinner(x2Model);
         SpinnerNumberModel y1Model = 
-	    new SpinnerNumberModel(-5.0, -40.0, 39.0, 0.1);
-        final JSpinner spinnerY1 = new JSpinner(y1Model);
+	    new SpinnerNumberModel(-10.0, -40.0, 39.0, 0.1);
+        spinnerY1 = new JSpinner(y1Model);
         SpinnerNumberModel y2Model = 
-	    new SpinnerNumberModel(5.0, -39.0, 40.0, 0.1);
-        final JSpinner spinnerY2 = new JSpinner(y2Model);
+	    new SpinnerNumberModel(10.0, -39.0, 40.0, 0.1);
+	spinnerY2 = new JSpinner(y2Model);
 	
-	final Controller c = new Controller("cos(x) / x");
+	//c = new Controller("3*sqr(((x-4)^2/16)-1)-1");
 
 	unitX = (int)(Double.parseDouble(x2Model.getValue().toString()) -
 		     Double.parseDouble(x1Model.getValue().toString()));
 
-	domain = unitX * 1000;
-
 	unitY = (int)(Double.parseDouble(y2Model.getValue().toString()) -
 		     Double.parseDouble(y1Model.getValue().toString()));
 
-	codomain = unitY * 1000;
+	String auxX1 = x1Model.getValue().toString(), 
+	    auxY1 = y1Model.getValue().toString(),
+	    auxX2 = x2Model.getValue().toString(), 
+	    auxY2 = y2Model.getValue().toString();
 
-	getXY(x1Model.getValue().toString(), x2Model.getValue().toString(), 
-	      y1Model.getValue().toString(), y2Model.getValue().toString(), c);
-	repaint();
-	       
-	/*
-        grafica.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                final Controller c = new Controller(functionT.getText());
-                String msg = c.getMessage();
-
-                if (msg != null) {
-                    JOptionPane.showMessageDialog(null, msg);
-                } else {
-                    getXY(spinnerX1, spinnerX2, spinnerY1, spinnerY2, c);
-		    repaint();
-                }
-            }
-        });
-
-        limpia.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                functionT.setText("");
-            }
-        });
-
-        spinnerW.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                final Controller c = new Controller(functionT.getText());
-                String msg = c.getMessage();
-
-                if (msg != null) {
-                    JOptionPane.showMessageDialog(null, msg);
-                } else {
-                    getXY(spinnerX1, spinnerX2, spinnerY1, spinnerY2, c);
-		    repaint();
-                }
-            }
-        });
+	minX = Double.parseDouble(auxX1);
+	minY = Double.parseDouble(auxY1);
+	maxX = Double.parseDouble(auxX2);
+	maxY = Double.parseDouble(auxY2);
 	
-	spinnerH.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                final Controller c = new Controller(functionT.getText());
-                String msg = c.getMessage();
-
-                if (msg != null) {
-                    JOptionPane.showMessageDialog(null, msg);
-                } else {
-                    getXY(spinnerX1, spinnerX2, spinnerY1, spinnerY2, c);
-		    repaint();
-                }
-            }
+        grafica.addActionListener(new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+		    c = new Controller(functionT.getText());
+		    String msg = c.getMessage();
+		    
+		    if (msg != null) {
+			JOptionPane.showMessageDialog(null, msg);
+		    } else {
+			setMinMaxValues();
+			getXY(minX, maxX, minY, maxY);
+			repaint();
+		    }
+		}
 	    });
-	*/
-	getContentPane().setPreferredSize(new Dimension(660, 620));
-	/*
-	FlowLayout f = new FlowLayout();
+	
+	
+        limpia.addActionListener(new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+		    functionT.setText("");
+		    c = new Controller(functionT.getText());
+		    coordinates = new ArrayList<Coordinates>();
+		    spinnerX1.setValue((Object)(-10));
+		    spinnerX2.setValue((Object)(10));
+		    spinnerY1.setValue((Object)(-10));
+		    spinnerY2.setValue((Object)(10));
+		    setMinMaxValues();
+		    getXY(minX, maxX, minY, maxY);
+		    repaint();
+		}
+	    });
+
+        spinnerX1.addChangeListener(this);
+	spinnerX2.addChangeListener(this);
+	spinnerY1.addChangeListener(this);
+	spinnerY2.addChangeListener(this);
+	
+	spinnerW.addChangeListener(new ChangeListener() {
+		@Override
+		public void stateChanged(ChangeEvent e) {
+		    
+		    int valueW = 
+			Integer.parseInt(widthModel.getValue().toString()),
+			valueH = 
+			Integer.parseInt(heightModel.getValue().toString());
+			
+		    getContentPane().setPreferredSize(new Dimension(valueW,
+								    valueH));
+		    setGraphWidth(valueW);
+		    if(c == null)
+			return;
+
+		    if(c.getMessage() != null)
+			return;
+		    
+		    getXY(minX, maxX, minY, maxY);
+		    repaint();
+		    
+		}
+	    });
+      
+	spinnerH.addChangeListener(new ChangeListener() {
+		@Override
+		public void stateChanged(ChangeEvent e) {
+
+		    int valueW = 
+			Integer.parseInt(heightModel.getValue().toString()),
+			valueH = 
+			Integer.parseInt(heightModel.getValue().toString());
+			
+		    getContentPane().setPreferredSize(new Dimension(valueW,
+								    valueH));
+		    setGraphHeight(valueH);
+		    if(c == null)
+			return;
+
+		    if(c.getMessage() != null)
+			return;
+		    
+		    getXY(minX, maxX, minY, maxY);
+		    repaint();
+
+		}
+	    });
+	
+	getContentPane().setPreferredSize(new Dimension(660, 620));	
+
+		FlowLayout f = new FlowLayout();
 	setLayout(f);
 	
 	add(function);
@@ -172,11 +220,12 @@ public class Graph extends JFrame {
         add(y2);
         add(spinnerY2);
 
-        add(grafica);
-        add(limpia);
-        add(svg);*/
+	add(grafica);
+	add(limpia);
+	add(svg);
 	
 	pack();
+	
         setResizable(false);
 	setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	setVisible(true);
@@ -204,18 +253,24 @@ public class Graph extends JFrame {
         g2d.setColor(new Color(red, green, blue));	
 	g2d.setStroke(new BasicStroke(2));
 
-	for(int i = 0; i < coordinates.size(); i++){
-	    Coordinates c = coordinates.get(i);	    
-
-	    if(Double.isNaN(c.getY()))
+	double x1, y1, x2, y2;
+	for(int i = 1; i < coordinates.size(); i++){
+	    Coordinates c1 = coordinates.get(i - 1);	    
+	    Coordinates c2 = coordinates.get(i);
+	    if(Double.isNaN(c1.getY()) || Double.isNaN(c2.getY()))
 		continue;	    
 	    
-	    x = c.getX();
-	    y = c.getY();	    
-	      
+	    x1 = c1.getX();
+	    y1 = c1.getY();	    
+	    x2 = c2.getX();
+	    y2 = c2.getY();	    
+
+	    if(Math.abs(x2 - x1) >= 0.02)
+		continue;
+	    
 	    Shape l = 
-		new Line2D.Double((factorX * x) + gW, (factorY * -y) + gH, 
-				  (factorX * x) + gW, (factorY * -y) + gH);
+		new Line2D.Double((factorX * x1) + gW, (factorY * -y1) + gH, 
+				  (factorX * x2) + gW, (factorY * -y2) + gH);
 	    g2d.draw(l);
 	}
     }
@@ -228,21 +283,67 @@ public class Graph extends JFrame {
 	g2d.drawLine(o1, 40 , o1, graphHeight + 39);	
     }
 
-    private void getXY(String x1, String x2, String y1, String y2, 
-		       Controller c) {
+    private void getXY(double x1, double x2, double y1, double y2) {
+        
+        int noOfPoints = (int) ((x2 - x1) * graphWidth + 1);        		
+	if(c.getMessage() != null)
+	    return;
 
-        double xMin = Double.parseDouble(x1), xMax = Double.parseDouble(x2),
-	    yMin = Double.parseDouble(y1), yMax = Double.parseDouble(y2);
-
-        int noOfPoints = (int) ((xMax - xMin) * graphWidth + 1);        	
-	
-	
-        for (; xMin <= xMax; xMin += 0.01) {                     
-	    double y = c.evaluate(xMin);	    
-	    if(y < yMin || y > yMax)
+        for (; x1 <= x2; x1 += 0.01) {                     
+	    double y = c.evaluate(x1);	    
+	    if(y < y1 || y > y2)
 		continue;
 
-	    coordinates.add(new Coordinates(xMin, y));
+	    coordinates.add(new Coordinates(x1, y));
         }	
     }   
+
+    @Override    
+    public void stateChanged(ChangeEvent e) {	        
+	
+	double s1 = 
+	    Double.parseDouble(spinnerX1.getModel().getValue().toString()),
+	    s2 = 
+	    Double.parseDouble(spinnerX2.getModel().getValue().toString()),
+	    s3 = 
+	    Double.parseDouble(spinnerY1.getModel().getValue().toString()),
+	    s4 = 
+	    Double.parseDouble(spinnerY2.getModel().getValue().toString());
+
+	if(s1 > s2){
+	    JOptionPane.showMessageDialog(null, "Verificar los valores de X");
+	    return;
+	}
+
+	if(s3 > s4){
+	    JOptionPane.showMessageDialog(null, "Verificar los valores de X");
+	    return;
+	}
+
+	setMinMaxValues();
+	if(c == null)
+	    return;
+
+	if(c.getMessage() != null)
+	    return;
+
+	getXY(minX, maxX, minY, maxY);
+	repaint();
+    }
+
+    private void setMinMaxValues(){
+
+	minX = Double.parseDouble(spinnerX1.getModel().getValue().toString());
+	maxX = Double.parseDouble(spinnerX2.getModel().getValue().toString());
+	minY = Double.parseDouble(spinnerY1.getModel().getValue().toString());
+	maxY = Double.parseDouble(spinnerY2.getModel().getValue().toString()); 
+    }
+
+    private void setGraphWidth(int value){
+	this.graphWidth = value - 20;	
+    }
+
+    private void setGraphHeight(int value){
+	this.graphHeight = value - 120;
+    }
 }
